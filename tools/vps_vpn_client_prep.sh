c8378d71d233fec4262f298d3b20d12c0f23f18d
Dmitry Makovey <dmakovey@yahoo.com>
2022-03-09 08:04:22 -0700

    quote the string

--- tools/vps_vpn_client_prep.sh
+++ tools/vps_vpn_client_prep.sh
@@ -10,7 +10,7 @@ RT_TABLES=${RT_TABLES:-/etc/iproute2/rt_tables}
 
 [ -z "${GATEWAY_IP}" ] && exit
 table_str="${IP_RULE_TABLE_NUM} ${IP_RULE_TABLE}"
-grep -qF ${table_str} ${RT_TABLES} || echo "${table_str}" >> ${RT_TABLES}
+grep -qF "${table_str}" ${RT_TABLES} || echo "${table_str}" >> ${RT_TABLES}
 ip rule show | grep fwmark && exit
 ip rule add fwmark ${FWMARK} table ${IP_RULE_TABLE}
 ip route add default via ${GATEWAY_IP} dev ${GATEWAY_IF} table ${IP_RULE_TABLE}
